89844e78e11be263517029cdd9a909de9e1ed655
Dark-Soleil <adonai.ouisol@epitech.eu>
2024-07-25 14:03:16 +0200

    manage user

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,35 +3,60 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/api/register', name: 'api_register', methods: ['POST'])]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface, EntityManagerInterface $em)
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface, EntityManagerInterface $entity)
     {
         try {
             $data = json_decode($request->getContent(), true);
 
-            $user = new User();
+            $errors = [];
 
+            if (empty($data['username'])) {
+                $errors[] = 'Entrez le username.';
+            }
+
+            if (empty($data['lastname'])) {
+                $errors[] = 'Entrez le lastname.';
+            }
+
+            if (empty($data['email'])) {
+                $errors[] = "Entrez l'email";
+            }
+
+            if (empty($data['password'])) {
+                $errors[] = 'Entrez le password.';
+            }
+
+            if (!empty($errors)) {
+                return $this->json(['errors' => $errors], 400);
+            }
+
+            $user = new User();
             $user->setEmail($data['email']);
             $user->setRoles(['ROLE_USER']);
-            $passwordHaser = $userPasswordHasherInterface->hashPassword($user, $data['password']);
-            $user->setPassword($passwordHaser);
+            $passwordHashed = $userPasswordHasherInterface->hashPassword($user, $data['password']);
+            $user->setPassword($passwordHashed);
+            $user->setUsername($data['username']);
+            $user->setLastname($data['lastname']);
+            $user->setCreatedAt(new \DateTimeImmutable()); 
+            $user->setUpdatedAt(new \DateTimeImmutable()); 
+            $user->setVerified(false); 
 
-            $em->persist($user);
-            $em->flush();
+            $entity->persist($user);
+            $entity->flush();
 
             return $this->json(['message' => 'Utilisateur enregistré avec succès'], 200);
         } catch (\Throwable $th) {
-            return $this->json(['message' => "Erreur impossible d'enregistrer l'utilisateur"], 404);
+            return $this->json(['message' => "Erreur, impossible d'enregistrer l'utilisateur"], 500);
         }
     }
 }
