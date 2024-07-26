6f7591083bacec7f86d7971a139ebc9381c5df51
Dark-Soleil <adonai.ouisol@epitech.eu>
2024-07-26 16:14:12 +0200

    Push Crud of user

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+ 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,23 +9,33 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
-
+ 
 class RegisterController extends AbstractController
 {
     #[Route('/api/register', name: 'api_register', methods: ['POST'])]
     public function register(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
-
+ 
+        if (empty($data['email']) || empty($data['password']) || empty($data['firstname']) || empty($data['lastname'])) {
+            return $this->json(['message' => 'Tous les champs sont requis'], 403);
+        }
+ 
         $user = new User();
         $user->setEmail($data['email']);
         $user->setRoles(['ROLE_USER']);
         $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
         $user->setPassword($hashedPassword);
-
+        $user->setFirstname($data['firstname']);
+        $user->setLastname($data['lastname']);
+        $user->setCreatedAt(new \DateTimeImmutable());
+        $user->setUpdatedAt(new \DateTimeImmutable());
+        $user->setVerified(false);
+        $user->setPicture($data['picture'] ?? null);
+ 
         $entityManager->persist($user);
         $entityManager->flush();
-
+ 
         return $this->json(['message' => 'Utilisateur enregistré avec succès'], 200);
     }
 }
