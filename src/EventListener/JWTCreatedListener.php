abd202d701df3b845109678078e91301f27e7f64
NightDev83 <dylan.bouraoui83@gmail.com>
2024-07-26 15:37:29 +0200

    JWT Creator

---
+++ src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+class JWTCreatedListener
+{
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        // $payload = $event->getData();
+        $user = $event->getUser();
+
+        $payload['email'] = $user->getEmail();
+
+        $event->setData($payload);
+    }
+}
