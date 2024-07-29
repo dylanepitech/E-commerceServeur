f58bf8b112819e1766e51b8963eab102016abeff
NightDev83 <dylan.bouraoui83@gmail.com>
2024-07-29 12:09:43 +0200

    push email

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Emails\EmailService;
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -12,19 +14,28 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class RegisterController extends AbstractController
 {
+    protected $EmailService;
+    public function __construct(EmailService $EmailService)
+    {
+        $this->EmailService = $EmailService;
+    }
+
     #[Route('/api/register', name: 'api_register', methods: ['POST'])]
     public function register(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): JsonResponse
     {
-        
+
         try {
-            
+
             $data = json_decode($request->getContent(), true);
-    
-    
+
+
             if (empty($data['email']) || empty($data['password']) || empty($data['firstname']) || empty($data['lastname'])) {
                 return $this->json(['message' => 'Tous les champs sont requis'], 403);
             }
+
             $user = new User();
+            $token = bin2hex(random_bytes(32));
+            $user->setToken($token);
             $user->setEmail($data['email']);
             $user->setRoles(['ROLE_USER']);
             $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
@@ -37,13 +48,42 @@ class RegisterController extends AbstractController
             $user->setPicture($data['picture'] ?? null);
             $user->setActif(true);
 
+            $this->EmailService->sendEmail(
+                $data['email'],
+                "Bienvenue chez Archideco !",
+                "emails/welcome.html.twig",
+                [
+                    "firstname" => $data["firstname"],
+                    "lastname" => $data["lastname"],
+                    "token" => $token
+                ]
+            );
+
             $entityManager->persist($user);
             $entityManager->flush();
-
         } catch (\Throwable $th) {
             return $this->json(['message' => "Erreur lors de l'inscription"], 500);
         }
 
         return $this->json(['message' => 'Utilisateur enregistré avec succès'], 200);
+    }
+
+    #[Route('/validate/{token}', name: 'validate_token', methods: ['GET'])]
+    public function validateEmail($token, UserRepository $userRepository, EntityManagerInterface $em)
+    {
+        try {
+            $user = $userRepository->findOneBy(['token' => $token]);
+
+            if ($user) {
+                $user->setVerified(true);
+                $em->persist($user);
+                $em->flush();
+                return $this->json(['message' => "Compte vérifier "], 201);
+            } else {
+                return $this->json(['message' => "Erreur pas d'utilisateur trouver"], 403);
+            }
+        } catch (\Throwable $th) {
+            return $this->json(['message' => "Erreur pas d'utilisateur trouver"], 403);
+        }
     }
 }
---
+++ src/Emails/EmailService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Emails;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Twig\Environment;
+
+class EmailService
+{
+    private $mailer;
+    private $twig;
+
+    public function __construct(MailerInterface $mailer, Environment $twig)
+    {
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+    }
+
+    public function sendEmail(string $to, string $subject, string $template, array $context): void
+    {
+        $htmlContent = $this->twig->render($template, $context);
+
+        $email = (new Email())
+            ->from('dylan.bouraoui83@gmail.com')
+            ->to($to)
+            ->subject($subject)
+            ->html($htmlContent);
+
+        $this->mailer->send($email);
+    }
+}
