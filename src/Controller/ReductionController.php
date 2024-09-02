8286e5fda724bf629fd5d8f99ad1881243cb3fea
NightDev83 <dylan.bouraoui83@gmail.com>
2024-09-02 15:19:44 +0200

    push rapide

--- src/Controller/ReductionController.php
+++ src/Controller/ReductionController.php
@@ -28,28 +28,29 @@ class ReductionController extends AbstractController
             if (empty($idProducts) || !$reductionValue || !$endAt) {
                 return $this->json(["message" => "Veuillez remplir tous les champs"], 400);
             }
-            $intvalID = intval($idProducts);
             $endAtDate = new \DateTimeImmutable($endAt);
             $createdAt = new \DateTimeImmutable();
 
-            $products = $productsRepository->find($intvalID);
+            foreach ($idProducts as $value) {
+                $products = $productsRepository->find($value);
 
-            $reduction = $products->getReduction();
+                $reduction = $products->getReduction();
 
-            if (!$reduction) {
-                $reduction = new Reduction();
-                $reduction->setReduction($reductionValue);
-                $reduction->setCreatedAt($createdAt);
-                $reduction->setEndAt($endAtDate);
-                $reduction->setIdCategory($products);
-                $products->setReduction($reduction);
-                $entityManager->persist($reduction);
-                $entityManager->flush();
-            } else {
-                $reduction->setReduction($reductionValue);
-                $entityManager->persist($reduction);
-                $entityManager->flush();
-                return $this->json('Code promo chaneger', 200);
+                if (!$reduction) {
+                    $reduction = new Reduction();
+                    $reduction->setReduction($reductionValue);
+                    $reduction->setCreatedAt($createdAt);
+                    $reduction->setEndAt($endAtDate);
+                    $reduction->setIdCategory($products);
+                    $products->setReduction($reduction);
+                    $entityManager->persist($reduction);
+                    $entityManager->flush();
+                } else {
+                    $reduction->setReduction($reductionValue);
+                    $entityManager->persist($reduction);
+                    $entityManager->flush();
+                    return $this->json('Code promo chaneger', 200);
+                }
             }
             return $this->json(["data" => ["Reductions mises à jour/créées", "success"]], 201);
         } catch (\Throwable $th) {
